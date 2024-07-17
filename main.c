b60e9cad8d0d3fd78b773db8fca263d7f54284e5
David Mutua <david.mutua@squaddigital.com>
2024-07-17 10:50:52 +0300

    demo loops and strings

--- main.c
+++ main.c
@@ -9,11 +9,21 @@ int main()
     // C uses character arrays to represent strings.
     char name[] = "World!";
 
+    char str[][16] = {"David", "Tom", "Emmanuella"};
+
     uint8_t num1 = 7;
     uint8_t num2 = 13;
 
-    char* fiftycharvalues[50];
+    char *fiftycharvalues[50];
     fiftycharvalues[0] = "Anorld";
+
+    int count = sizeof(str) / sizeof(str[0]);
+
+    // loops
+    for (int i = 0; i < count; i++)
+    {
+        printf("%s\n", str[i]);
+    }
 
     printf("Hello %s, %s\n\n", getName(name), fiftycharvalues[0]);
 
